2b0d705f19a51e9eb9804a810295f8ead021cb6f
Kevin Bonds <kevin.w.bonds@gmail.com>
2017-05-26 10:02:31 -0700

    Initial commit of Vendor Tier app to CF

---
+++ init.r
@@ -0,0 +1,10 @@
+install.packages("Cairo", dependencies = TRUE)
+install.packages("tidyr", dependencies = TRUE)
+install.packages("shiny", dependencies = TRUE)
+install.packages("PivotalR", dependencies = TRUE)
+install.packages("lubridate", dependencies = TRUE)
+install.packages("shinythemes", dependencies = TRUE)
+install.packages("readr", dependencies = TRUE)
+
+
+options(device='cairo')
---
+++ startscript.R
@@ -0,0 +1,6 @@
+library(shiny)
+
+port <- Sys.getenv('PORT') 
+print(port)
+
+shiny::runApp('app', host = '0.0.0.0', port = as.numeric(port))
